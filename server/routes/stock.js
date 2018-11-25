84da9d7724f63b254b11bf9844dd8d37dfcd82e2
Anshul Dubey <anshuldubey2166@gmail.com>
2018-11-26 00:57:00 +0530

    Response working now

--- server/routes/stock.js
+++ server/routes/stock.js
@@ -2,13 +2,17 @@ require("dotenv").config();
 var express = require("express");
 var router = express.Router();
 let yourApiKey = process.env.Alpha_Key;
-const alpha = require("alphavantage")({ key: ""+yourApiKey });
+const alpha = require("alphavantage")({ key: "" + yourApiKey });
 
 router.get("/", (req, res, next) => {
   console.log("Here");
-  alpha.data.batch([`msft`, `aapl`]).then(data => {
-    console.log(data);
-  });
+  alpha.data
+    .batch([`msft`, `aapl`])
+    .then(data => {
+      console.log(data);
+      res.json({ status: "success", message: data });
+    })
+    .catch(err => res.json({ status: "failed", message: err }));
 });
 
 module.exports = router;
