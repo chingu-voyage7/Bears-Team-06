f13bccbbbb3b0d07a4193d0a3d2ca13b9d1f60f4
Anshul Dubey <anshuldubey2166@gmail.com>
2018-11-26 01:10:14 +0530

    Taking params from url

--- server/routes/stock.js
+++ server/routes/stock.js
@@ -6,8 +6,10 @@ const alpha = require("alphavantage")({ key: "" + yourApiKey });
 
 router.get("/", (req, res, next) => {
   console.log("Here");
+  let symbol_arr = JSON.parse(req.query.symbols);
+  console.log(symbol_arr);
   alpha.data
-    .batch([`msft`, `aapl`])
+    .batch(symbol_arr)
     .then(data => {
       console.log(data);
       res.json({ status: "success", message: data });
