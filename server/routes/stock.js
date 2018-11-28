75cb8c63660e426f248ab8ed8a40af034f540001
Anshul Dubey <anshuldubey2166@gmail.com>
2018-11-29 00:14:28 +0530

    Authenticate with stock working

--- server/routes/stock.js
+++ server/routes/stock.js
@@ -2,10 +2,23 @@ require("dotenv").config();
 var express = require("express");
 var router = express.Router();
 let yourApiKey = process.env.Alpha_Key;
+let User = require("../models/users");
 const alpha = require("alphavantage")({ key: "" + yourApiKey });
+let userObj = null;
+
+router.use(function(req, res, next) {
+	//get the user details from current session
+	User.findById(req.session.passport.user, function(err, user) {
+		userObj = user;
+	})
+		.then(r => {
+			next();
+		})
+		.catch(err => next(err));
+});
 
 router.get("/", (req, res, next) => {
-  console.log("Here");
+  console.log("Here inside stocks");
   let symbol_arr = JSON.parse(req.query.symbols);
   console.log(symbol_arr);
   alpha.data
