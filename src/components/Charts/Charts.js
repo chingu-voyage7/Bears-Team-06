d867b0d9d0dd7cf7c3944fb1ace16ba4152c737a
Anshul Dubey <anshuldubey2166@gmail.com>
2018-12-08 02:25:43 +0530

    Charts done

---
+++ src/components/Charts/Charts.js
@@ -0,0 +1,42 @@
+import React, { Component } from "react";
+import { Chart } from "react-charts";
+import { connect } from "react-redux";
+import { bindActionCreators } from "redux";
+import * as chartActions from "../../store/actions/charts/charts";
+
+class Charts extends Component {
+  componentDidMount(){
+    this.props.charts.getCharts();
+  }
+  render() {
+    let data = this.props.chart;
+    return (
+      <div
+        style={{
+          width: "400px",
+          height: "300px"
+        }}
+      >
+        <Chart
+          data={data}
+          axes={[
+            { primary: true, type: "linear", position: "bottom" },
+            { type: "linear", position: "left" }
+          ]}
+        />
+      </div>
+    );
+  }
+}
+
+const mapStateToProps = state => ({
+  chart: state.chart
+});
+
+const mapActionsToProps = dispatch => {
+  return {
+    charts: bindActionCreators(chartActions, dispatch)
+  };
+};
+
+export default connect(mapStateToProps,mapActionsToProps)(Charts);
