c628a39dad30ab533d9a9c3d5f49eefad3fc965d
samratluitel <ngc2058@gmail.com>
2018-12-25 22:51:10 +0545

    Side nav added on landing page

--- src/components/Landing/Landing.js
+++ src/components/Landing/Landing.js
@@ -5,11 +5,13 @@ import LandingWorkArea from "./LandingWorkArea/LandingWorkArea";
 import PartnerArea from "./PartnerArea/PartnerArea";
 import ContactUs from "./ContactUs/ContactUs";
 import LandingFooter from "../common/LandingFooter/LandingFooter";
+import LandingSideNav from "../common/LandingSideNav/LandingSideNav";
 
 class Landing extends Component {
   render() {
     return (
       <div className="Landing">
+        <LandingSideNav />
         <LandingHeader />
         <LandingHero />
         <LandingWorkArea />
---
+++ src/components/common/LandingSideNav/LandingSideNav.js
@@ -0,0 +1,39 @@
+import React, { Component, Fragment } from "react";
+import classnames from "classnames";
+
+class LandingSideNav extends Component {
+  state = {
+    open: false,
+  };
+
+  onSideNavOpen = () => this.setState({ open: true });
+  onSideNavClose = () => this.setState({ open: false });
+  render() {
+    return (
+      <div className={"LandingSideNav"}>
+        {this.state.open && (
+          <div className="LandingSideNav__btn" onClick={this.onSideNavClose}>
+            <i class="fas fa-times" />
+          </div>
+        )}
+        {!this.state.open && (
+          <div className="LandingSideNav__btn" onClick={this.onSideNavOpen}>
+            <i class="fas fa-bars" />
+          </div>
+        )}
+        <div
+          className={classnames({
+            "LandingSideNav__nav-lists": true,
+            "LandingSideNav__nav-lists--close": !this.state.open,
+          })}
+        >
+          <div className="LandingSideNav__single-nav">Home</div>
+          <div className="LandingSideNav__single-nav">How To</div>
+          <div className="LandingSideNav__single-nav">Partners</div>
+          <div className="LandingSideNav__single-nav">Contact Us</div>
+        </div>
+      </div>
+    );
+  }
+}
+export default LandingSideNav;
