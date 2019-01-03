2fbca246904ba37f6a248a53ceb14b0923d3778a
Anshul Dubey <anshuldubey2166@gmail.com>
2019-01-03 10:21:44 +0530

    Added my-stocks and following-stocks

--- src/components/Profile/Profile.js
+++ src/components/Profile/Profile.js
@@ -4,29 +4,46 @@ import CardMedia from "@material-ui/core/CardMedia";
 import Typography from "@material-ui/core/Typography";
 import ListBox from "./ListBox/ListBox";
 import "./profile.css";
+import StockTable from "../StockTable/StockTable";
+// Sample data for StockTable
+const data = [
+  ["AZ", 495, 96, 2140],
+  ["BZ", 100, -12, 11120],
+  ["XZ", 450, 40, 66063],
+  ["RZ", 305, 540, 1120],
+  ["TZ", 506, 50, 1055],
+  ["YZ", 103, -119, 24455],
+  ["GZ", 110, 12, 3636],
+];
+
+const columns = ["Symbol", "Price", "% Change", "$ Volume"];
+
 
 class Profile extends Component {
   render() {
     return (
-      <div className="profile-stock">
+      <div className="profile">
         <Grid container spacing={24}>
           <Grid item xs={12} sm={6} md={4} lg={2}>
             <CardMedia
               component="img"
               alt="Profile Pic"
-              image="/assets/alberto.png"
+              image="/assets/images/alberto.png"
               title="Profile Pic"
               className="image-round"
-              width="150"
-              height="150"
+              width="200"
+              height="200"
             />
           </Grid>
           <Grid item xs={12} sm={6} md={8} lg={10}>
             <div className="profile-info">
-            <Typography variant="h5" component="h2">
+              <Typography variant="h5" component="h2" className="profile-text">
                 Anshul
               </Typography>
-              <Typography variant="p" component="p">
+              <Typography variant="p" component="p" className="profile-text">
+                DOB: 19th July, 1997
+              </Typography>
+              <Typography variant="p" component="p" className="profile-text">
                 Add a small bio here
               </Typography>
             </div>
@@ -35,18 +52,15 @@ class Profile extends Component {
         <div className="all-lists">
           <Grid container spacing={24}>
             <Grid item xs={12} sm={12} md={10} lg={11}>
-              <ListBox title="Skills" data="React, NodeJs, Python"/>
-            </Grid>
-            <Grid item xs={12} sm={12} md={10} lg={11}>
-              <ListBox title="Accomplishments" data="First in district coding championship, Third rank in college"/>
-            </Grid>
-            <Grid item xs={12} sm={12} md={10} lg={11}>
-              <ListBox title="Projects" data="Built a question paper generator using Java, Built an image modifier using Javascript"/>
-            </Grid>
-            <Grid item xs={12} sm={12} md={10} lg={11}>
-              <ListBox title="Completed Courses" data="An introduction to computer science, Data structures and alogorithms"/>
+              <ListBox title="Following stocks" data="Google, Microsoft, Apple, Flipkart, Amazon, PayPal"/>
             </Grid>
           </Grid>
+        </div>
+        <div className="all-stocks">
+        <Typography variant="h4" component="h2" className="profile-text text-center">
+                My Stocks
+              </Typography>
+          <StockTable columns={columns} data={data} />
         </div>
       </div>
     );
